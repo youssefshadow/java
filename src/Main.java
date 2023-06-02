1c243546a45615b3ef2ad033b894c64d91ab53d1
youssefshadow <youssef20ben@gmail.com>
2023-06-02 10:21:32 +0200

    first variable

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
+        int nbr1 = 10;
+        int nbr2 = 5 ;
+        System.out.println(nbr1 + nbr2);
 
     }
-    boolean active = true;
-    int nbr = 10;
+
 
 }
