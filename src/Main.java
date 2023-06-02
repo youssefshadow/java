5383a239936b380485fb7ef8a4fe9d5a89175df3
youssefshadow <youssef20ben@gmail.com>
2023-06-02 12:30:26 +0200

    scanner with tva

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Veuillez saisir  la valeur de i :");
+        System.out.println("Veuillez saisir la valeur de i :");
         int i = scanner.nextInt();
 
-        System.out.println("Veuillez saisir  la valeur de j :");
+        System.out.println("Veuillez saisir la valeur de j :");
         int j = scanner.nextInt();
 
-        System.out.println("Veuillez saisir  la valeur de k :");
+        System.out.println("Veuillez saisir la valeur de k :");
         int k = scanner.nextInt();
 
         int resultat = subtraction(i, j, k);
 
         System.out.println("Le résultat de la soustraction est égal à : " + resultat);
+
+        System.out.println("Veuillez saisir le prix hors taxe :");
+        double montantHT = scanner.nextDouble();
+
+        System.out.println("Veuillez saisir le taux de TVA :");
+        double tauxTVA = scanner.nextDouble();
+
+        System.out.println("put the quantities  :");
+        int quantite = scanner.nextInt();
+
+        double calcul = calculerPrixTTC(montantHT, quantite, tauxTVA);
+        System.out.println("Le prix TTC est : " + calcul);
     }
-
-
-
 
     public static int subtraction(int i, int j, int k) {
         return i - j - k;
     }
 
-    public static void intervertir(int i, int j){
-        int change = i;
-        i = j;
-        j = change;
+    public static double calculerPrixTTC(double montantHT, int quantite, double tauxTVA) {
+        double montantTVA = montantHT * (tauxTVA / 100);
+        double prixTTC = montantHT + montantTVA;
+        return prixTTC;
     }
 }
