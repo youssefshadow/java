718340a5ec96e22922cc1791342437ae7257a6e1
youssefshadow <youssef20ben@gmail.com>
2023-06-06 12:11:27 +0200

    arrayList++

--- src/Exemple.java
+++ src/Exemple.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class Exemple {
     public static void main(String[] args) {
         int[] intArray = {24, 32, 0, 34, 12, 210, 2};
+        int list;
+        List<Integer> sortedList = new ArrayList<>();
+        sortedList.add(5);
+        sortedList.add(9);
+        sortedList.add(-2);
+        sortedList.add(-41);
+        System.out.println(getMin(sortedList));
+
 
         int maxNum = intArray[0];
 
@@ -15,14 +23,9 @@ public class Exemple {
 
         System.out.println("le plus grand nombre  = " + maxNum);
     }
-    public static Integer getMin(List<Integer> list)
+    public static Integer getMin(List<Integer> sortedList)
     {
-        int[] intArray = {24, 32, 0, 34, 12, 210, 2};
-        List<Integer> sortedList = new ArrayList<>(list);
-        sortedList.add(5);
-        sortedList.add(9);
-        sortedList.add(-2);
-        sortedList.add(41);
+
 
         Collections.sort(sortedList);
 
